5206f91d9a986c3b02c8feb7def31a816b22d7b4
sparekh315 <saniaparekh315@gmail.com>
2020-08-21 19:36:59 -0500

    lotto and mini lotto game implemented

---
+++ lotto-game/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Ball from './Components/Ball';
+import Lottery from './Components/Lottery';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <h1>Lotto Game!</h1>
+      <Lottery />
+      <Lottery title='Mini Daily' maxNum={10} numBalls={4}/> 
+    </div>
+  );
+}
+
+export default App;
